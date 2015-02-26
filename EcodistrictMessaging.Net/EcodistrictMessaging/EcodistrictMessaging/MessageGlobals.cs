5b0abd1c870719a3874a488c2c232ab3986a5244
unknown <PerBe@SP01923.sp.se>
2015-02-26 14:22:58 +0100

    New files added for messaging

---
+++ EcodistrictMessaging.Net/EcodistrictMessaging/EcodistrictMessaging/MessageGlobals.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ecodistrict.Messaging
+{
+    public static class MessageGlobals
+    {
+        public enum EMethod
+        {
+            GetModules,
+            SelectModel,
+            StartModel
+        }
+
+        public enum EType
+        {
+            Request,
+            Response,
+            Result
+        }
+
+        public static iMessage ParseJsonMessage(string message)
+        {
+           return new GetModulesMessageRequest();
+        }
+    }
+}
