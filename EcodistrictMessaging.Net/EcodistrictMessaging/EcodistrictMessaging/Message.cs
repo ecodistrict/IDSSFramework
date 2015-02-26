166d82dcf7b869dcdb2f89f9a51ca5d5714e6374
unknown <PerBe@SP01923.sp.se>
2015-02-26 16:55:04 +0100

    New classes for reciving commands

---
+++ EcodistrictMessaging.Net/EcodistrictMessaging/EcodistrictMessaging/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ecodistrict.Messaging
+{
+    public interface iMessage
+    {
+        MessageGlobals.EMethod Method { get; set; }
+        MessageGlobals.EType Type { get; set; }
+
+        string ToJsonMessage();
+
+
+    }
+}
