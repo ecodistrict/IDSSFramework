49e4848897e9f19afdf17c4f2dfa35e6e4608898
Rasmus Einarsson <rasmus.einarsson@sp.se>
2015-04-09 16:51:27 +0200

    testing

--- schema/test.py
+++ schema/test.py
@@ -2,18 +2,17 @@ import jsonschema
 from jsonschema import validate
 import json
 import urllib.request
-#schema_url = "https://raw.githubusercontent.com/ecodistrict/IDSSFramework/master/schema/message.json"
-#schema = json.loads(urllib.request.urlopen(schema_url).read().decode('utf-8'))
-schema = json.loads(open("message.json", 'rb').read().decode("utf-8"))
+schema_url = "https://raw.githubusercontent.com/ecodistrict/IDSSFramework/master/schema/inputSpecification.json"
+schema = json.loads(urllib.request.urlopen(schema_url).read().decode('utf-8'))
+#schema = json.loads(open("message.json", 'rb').read().decode("utf-8"))
 
 json = {
   "method": "selectModule",
   "type": "response",
   "moduleId": "foo-bar_cheese-module-v1-0",
-  "kpiId": "cheese-taste-kpi",
+  "kpiId": "3cheese-taste-kpi",
   "variantId": "503f191e8fcc19729de860ea",
   "inputSpecification": {
-    "aoeu": 3,
     "age": {
       "label": "Age",
       "min": 0,
@@ -41,5 +40,32 @@ json = {
     }
   }
 }
-
+json = {
+    "aoeu": "3",
+    "age": {
+      "label": "Age",
+      "min": 0,
+      "type": "number",
+      "unit": "years"
+    },
+    "cheese-type": {
+      "label": "Cheese type",
+      "type": "select",
+      "value": "brie-cheese",
+      "options": [
+        {
+          "value": "alp-cheese",
+          "label": "Alpk\u00e4se"
+        },
+        {
+          "value": "edam-cheese",
+          "label": "Edammer"
+        },
+        {
+          "value": "brie-cheese",
+          "label": "Brie"
+        }
+      ]
+    }
+  }
 validate(json, schema)
