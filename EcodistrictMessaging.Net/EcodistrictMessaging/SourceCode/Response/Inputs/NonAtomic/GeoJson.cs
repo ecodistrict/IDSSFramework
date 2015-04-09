37c4273f91a80dee22634dfda45fd5dd6dc3b7da
Joma564 <johannes.markstrom@sp.se>
2015-04-09 14:10:03 +0200

    New input type: GeoJson

---
+++ EcodistrictMessaging.Net/EcodistrictMessaging/SourceCode/Response/Inputs/NonAtomic/GeoJson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Ecodistrict.Messaging
+{
+    [DataContract]
+    public class GeoJson : NonAtomic
+    {
+        [DataMember]
+        string geometryObject = "polygon";
+
+        internal GeoJson() { }
+
+        public GeoJson(string label, object order = null)
+        {
+            this.type = "geojson";
+            this.label = label;
+            this.order = order;
+        }
+
+    }
+}
