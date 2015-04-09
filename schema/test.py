28a42fd95368e9d6a57555bf319250292aa306ed
Rasmus Einarsson <rasmus.einarsson@sp.se>
2015-04-09 16:14:26 +0200

    Add checks for all message types
    Excluding inputSpecification so far

--- schema/test.py
+++ schema/test.py
@@ -2,14 +2,24 @@ import jsonschema
 from jsonschema import validate
 import json
 import urllib.request
-schema_url = "https://raw.githubusercontent.com/ecodistrict/IDSSFramework/master/schema/message.json"
-schema = json.loads(urllib.request.urlopen(schema_url).read().decode('utf-8'))
+#schema_url = "https://raw.githubusercontent.com/ecodistrict/IDSSFramework/master/schema/message.json"
+#schema = json.loads(urllib.request.urlopen(schema_url).read().decode('utf-8'))
+schema = json.loads(open("message.json", 'rb').read().decode("utf-8"))
+
 json = {
-    "method": "getModules",
-    "type": "request"
+  "method": "moduleResult",
+  "type": "result",
+  "moduleId": "foo-bar_cheese-module-v1-0",
+  "variantId": "503f191e8fcc19729de860ea",
+  "kpiId": "cheese-taste-kpi",
+  "outputs": [
+    {
+      "type": "kpi",
+      "value": 42,
+      "info": "Cheese tastiness",
+      "unit": "ICQU (International Cheese Quality Units)"
+    }
+  ]
 }
 
-try:
-    validate(json, schema)
-except jsonschema.exceptions.ValidationError as e:
-    print(e)
+validate(json, schema)
