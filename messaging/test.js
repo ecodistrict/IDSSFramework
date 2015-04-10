84451ef16eed6666ae9b4ff61300cd17ebb5ba70
Rasmus Einarsson <rasmus.einarsson@sp.se>
2015-04-10 13:34:48 +0200

    test

--- messaging/test.js
+++ messaging/test.js
@@ -2,14 +2,14 @@ var JaySchema = require('jayschema');
 var js = new JaySchema(JaySchema.loaders.http);     // we provide the HTTP loader here
                                                     // you could load from a DB instead
 
-//var instance = { "method": "selectModule", "type": "response" };
-var instance = "123aoeu";
-var schema = { "$ref": "https://github.com/ecodistrict/IDSSFramework/raw/master/messaging/schema.json#/web-friendly-string" };
+var baseURI = "https://github.com/ecodistrict/IDSSFramework/raw/master/messaging/schema.json"
+var instance = { "method": "getModules", "type": "request" };
+var schema = { "$ref": baseURI + "#/getModulesRequest" };
 
 js.validate(instance, schema, function(errs) {
-  if (errs) { console.error(errs); 
-    if (errs[0].subSchemaValidationErrors) {
-      console.log(errs[0].subSchemaValidationErrors['#/definitions/getModulesRequest']);
-    }}
-  else { console.log('validation OK!'); }
+  if (errs) {
+    console.error(errs);
+  } else { 
+    console.log('validation OK!');
+  }
 });
